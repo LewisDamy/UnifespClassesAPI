2f391962e52617be01d86f97e2e14fc176fc6635
LewisDamy <ldamy16@gmail.com>
2022-08-11 10:51:20 -0300

    making comments and added requirements file

--- unifespClasses/serializers.py
+++ unifespClasses/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from unifespClasses.models import Subject
 
-class SubjectSerializer(serializers.ModelSerializer):
+class SubjectSerializer(serializers.ModelSerializer):   # create class serializer
     class Meta:
-        model = Subject
-        fields = [
+        model = Subject     # point to the subject model
+        fields = [      # indicate which the fields in subject model
             'subjectID',
             'subjectName',
             'category',
